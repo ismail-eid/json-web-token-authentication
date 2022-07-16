840771cbf04354daa19c45e0cdf97ba449bf0845
Ismail Eid <ismacileid122@gmail.com>
2022-07-16 14:10:13 +0300

    created tables

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { config } = require('dotenv');
+config({ path: __dirname + '/.env' })
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
